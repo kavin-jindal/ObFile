bce7131f6a9cb71e3fc32fc79e1769e927a1b3d3
kavinjindal <68228966+kavinjindal@users.noreply.github.com>
2020-09-10 13:50:50 +0530

    Update setup.py

--- ObFile/setup.py
+++ ObFile/setup.py
@@ -4,10 +4,9 @@ try:
     import re
     import subprocess
 except ImportError:
-    print('Error Occured')
 os.system('pip install pyinstaller')
 os.system('pip install pyarmor')
 os.system('pip install colorama')
 os.system('pip install pyfiglet')
 os.system('Msgbox\setupmsg.vbs')
-os.system('ObFile.py')
+os.system('python ObFile.py')
